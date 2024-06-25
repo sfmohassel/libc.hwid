4c500629890056c0f180a54fada1afae5bf07864
Diego Garcia <dagarciam1014@gmail.com>
2024-06-24 22:09:15 -0400

    Added new options and improved functionality

--- libc.hwid.tests/HwIdTests.cs
+++ libc.hwid.tests/HwIdTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using Xunit;
 
 namespace libc.hwid.tests
@@ -14,7 +15,25 @@ namespace libc.hwid.tests
             var hardwareId2 = HwId.Generate();
             Assert.True(hardwareId2.Length == 40);
 
+            var hardwareIdMac = HwId.Generate(null, true);
+            Assert.True(hardwareIdMac.Length == 40);
+
             Assert.True(hardwareId1.Equals(hardwareId2, StringComparison.Ordinal));
+            Assert.False(hardwareId1.Equals(hardwareIdMac, StringComparison.Ordinal));
+
+            var sha256 = SHA256.Create();
+
+            var hardwareId1Sha256 = HwId.Generate(sha256);
+            Assert.True(hardwareId1.Length == 64);
+
+            var hardwareId2Sha256 = HwId.Generate(sha256);
+            Assert.True(hardwareId1.Length == 64);
+
+            var hardwareIdMacSha256 = HwId.Generate(sha256, true);
+            Assert.True(hardwareId1.Length == 64);
+
+            Assert.True(hardwareId1Sha256.Equals(hardwareId2Sha256, StringComparison.Ordinal));
+            Assert.False(hardwareId1Sha256.Equals(hardwareIdMacSha256, StringComparison.Ordinal));
         }
     }
 }
